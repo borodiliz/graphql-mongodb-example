6f2f5443321b9e4a113067ac1f8c4bd8b1917de5
gongzza <gz.us.to@gmail.com>
2018-09-16 14:41:42 +0900

    Migrate to typescript

--- src/start.ts
+++ src/start.ts
@@ -1,25 +1,39 @@
-import {MongoClient, ObjectId} from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import express from 'express'
 import bodyParser from 'body-parser'
-import {graphqlExpress, graphiqlExpress} from 'graphql-server-express'
-import {makeExecutableSchema} from 'graphql-tools'
-import cors from 'cors'
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
 
 const URL = 'http://localhost'
 const PORT = 3001
-const MONGO_URL = 'mongodb://localhost:27017/blog'
+const MONGO_URL = 'mongodb://localhost:27017/'
 
 const prepare = (o) => {
   o._id = o._id.toString()
   return o
 }
 
+interface Comment {
+  _id: string
+  postId: string
+  content: string
+  post: Post
+}
+
+interface Post {
+  _id: string
+  title: string
+  content: string
+  comments: [Comment]
+}
+
 export const start = async () => {
   try {
-    const db = await MongoClient.connect(MONGO_URL)
+    const client = await MongoClient.connect(MONGO_URL)
+    const db = client.db('blog')
 
-    const Posts = db.collection('posts')
-    const Comments = db.collection('comments')
+    const Posts = db.collection<Post>('posts')
+    const Comments = db.collection<Comment>('comments')
 
     const typeDefs = [`
       type Query {
@@ -51,40 +65,41 @@ export const start = async () => {
         query: Query
         mutation: Mutation
       }
-    `];
+    `]
 
     const resolvers = {
       Query: {
-        post: async (root, {_id}) => {
-          return prepare(await Posts.findOne(ObjectId(_id)))
+        post: async (root, { _id }: Post) => {
+          return prepare(await Posts.findOne(new ObjectId(_id)))
         },
         posts: async () => {
           return (await Posts.find({}).toArray()).map(prepare)
         },
-        comment: async (root, {_id}) => {
-          return prepare(await Comments.findOne(ObjectId(_id)))
-        },
+        comment: async (root, { _id }: Comment) => {
+          return prepare(await Comments.findOne(new ObjectId(_id)))
+        }
       },
       Post: {
-        comments: async ({_id}) => {
-          return (await Comments.find({postId: _id}).toArray()).map(prepare)
+        comments: async ({ _id }) => {
+          return (await Comments.find({ postId: _id }).toArray()).map(prepare)
         }
       },
       Comment: {
-        post: async ({postId}) => {
-          return prepare(await Posts.findOne(ObjectId(postId)))
+        post: async ({ postId }: Comment) => {
+          return prepare(await Posts.findOne(new ObjectId(postId)))
         }
       },
       Mutation: {
         createPost: async (root, args, context, info) => {
-          const res = await Posts.insert(args)
-          return prepare(await Posts.findOne({_id: res.insertedIds[0]}))
+          const res = await Posts.insertOne(args)
+          console.log(res)
+          return prepare(await Posts.findOne({ _id: res.insertedId }))
         },
         createComment: async (root, args) => {
           const res = await Comments.insert(args)
-          return prepare(await Comments.findOne({_id: res.insertedIds[0]}))
-        },
-      },
+          return prepare(await Comments.findOne({ _id: res.insertedId }))
+        }
+      }
     }
 
     const schema = makeExecutableSchema({
@@ -94,9 +109,7 @@ export const start = async () => {
 
     const app = express()
 
-    app.use(cors())
-
-    app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+    app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 
     const homePath = '/graphiql'
 
