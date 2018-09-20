d35d0827b588eccfa65ad0f9181071c966502e76
gongzza <gz.us.to@gmail.com>
2018-09-20 10:38:02 +0900

    Add scalar ObjectId

--- src/start.ts
+++ src/start.ts
@@ -2,6 +2,7 @@ import { MongoClient, ObjectId } from 'mongodb'
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
 import { Post, Comment } from './models'
+import { GraphQLScalarType } from 'graphql'
 
 require('dotenv').config()
 
@@ -15,6 +16,8 @@ export const createServer = async () => {
     const Comments = db.collection<Comment>('comments')
 
     const typeDefs = gql`
+      scalar ObjectId
+
       type Query {
         post(_id: String): Post
         posts: [Post]
@@ -22,14 +25,14 @@ export const createServer = async () => {
       }
 
       type Post {
-        _id: ID!
+        _id: ObjectId!
         title: String!
         content: String
         comments: [Comment!]
       }
 
       type Comment {
-        _id: ID!
+        _id: ObjectId!
         postId: String!
         content: String!
         post: Post
@@ -47,25 +50,27 @@ export const createServer = async () => {
     `
 
     const resolvers = {
+      ObjectId: new GraphQLScalarType({
+        name: 'ObjectId',
+        serialize: val => new ObjectId(val)
+      }),
       Query: {
         post: (root, { _id }: Post) => {
-          return Posts.findOne(new ObjectId(_id))
+          return Posts.findOne(_id)
         },
         posts: () => {
           return Posts.find({}).toArray()
         },
         comment: (root, { _id }: Comment) => {
-          return Comments.findOne(new ObjectId(_id))
+          return Comments.findOne(_id)
         }
       },
       Post: {
-        _id: ({ _id }: Post) => _id.toString(),
         comments: ({ _id }: Post) => {
           return Comments.find({ postId: _id.toString() }).toArray()
         }
       },
       Comment: {
-        _id: ({ _id }: Comment) => _id.toString(),
         post: ({ postId }: Comment) => {
           return Posts.findOne(new ObjectId(postId))
         }
